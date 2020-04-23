7a8c2114e756c5696294793892793a91e927592a
Nileshlad <ladnilesh1111@gmail.com>
2020-04-23 16:44:51 +0530

    Modified: Park and unpark method check vehicle present or not

--- src/main/java/parkinglotsystem/observer/IObservable.java
+++ src/main/java/parkinglotsystem/observer/IObservable.java
@@ -1,6 +1,12 @@
 package parkinglotsystem.observer;
 
+import parkinglotsystem.exception.ParkingLotException;
+import parkinglotsystem.model.Vehicle;
+
+import java.util.Map;
+
 public interface IObservable {
     public void update(Object o);
 
+    void isAvailable(Map<String, Vehicle> parkingLot, int parkingLotCapacity) throws ParkingLotException;
 }
--- src/main/java/parkinglotsystem/observer/Owner.java
+++ src/main/java/parkinglotsystem/observer/Owner.java
@@ -1,5 +1,11 @@
 package parkinglotsystem.observer;
 
+import parkinglotsystem.exception.ParkingLotException;
+import parkinglotsystem.model.Vehicle;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Owner implements IObservable {
     private String parkingLotStatus;
 
@@ -14,5 +20,18 @@ public class Owner implements IObservable {
 
     public void setParkingLotStatus(String isFull) {
         this.parkingLotStatus = isFull;
+    }
+
+    @Override
+    public void isAvailable(Map<String, Vehicle> parkingLot, int parkingLotCapacity) throws ParkingLotException {
+        if (!parkingLot.containsValue(null)) {
+            throw new ParkingLotException("Parking lot is full", ParkingLotException.ExceptionType.PARKING_LOT_IS_FULL);
+        }
+    }
+
+    public void isPresent(LinkedHashMap<String, Vehicle> parkingLot, Vehicle vehicle) throws ParkingLotException {
+        if (vehicle == null) {
+            throw new ParkingLotException("No such a vehicle", ParkingLotException.ExceptionType.NO_SUCH_A_VEHICLE);
+        }
     }
 }
